40785a73220fe0aabfe01ae76ca2073dd343ad1c
Matiko-Codes <mmatiko243@gmail.com>
2022-09-21 06:25:10 +0000

    Task 3 fix

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,43 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - a function that
- * inserts a node as the right-child of another node
- * @parent: a pointer to the node to insert the left-child in
- * @value: the value to store in the new node
- *
- * Return: returns a pointer to the created node,
- * or NULL on failure or if parent is NULL
+ * binary_tree_delete - function delete tree
+ * @tree: pointer to the parent node of the node to delete
+ * Return: Nothing
  */
-
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *new_node;
+	if (tree == NULL)
+		return;
 
-	if (!parent)
-	{
-		return (NULL);
-	}
-	new_node = malloc(sizeof(binary_tree_t));
-
-	if (!new_node)
-	{
-		return (NULL);
-	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
-	else
-	{
-		new_node->right = NULL;
-	}
-	parent->right = new_node;
-	return (new_node);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
 
